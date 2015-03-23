4fbbf011d975a14f5cf44823f30a23443335d476
Michael Woodward <michael@wearejh.com>
2015-03-23 22:25:49 +0000

    Start DiffServiceTest

---
+++ test/Service/DiffServiceTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace MikeyMike\RfcDigestor\Service;
+
+/**
+ * Class DiffServiceTest
+ *
+ * @package MikeyMike\RfcDigestor\Service
+ * @author  Michael Woodward <mikeymike.mw@gmail.com>
+ */
+class DiffServiceTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var DiffService
+     */
+    protected $diffService;
+
+    public function setUp()
+    {
+        $this->diffService = new DiffService();
+    }
+
+    public function testListDiffReturnsEmptyArrayWhenNoDiff()
+    {
+        $list1 = [
+            'In voting phase' => [
+                'In Operator'          => ['In Operator', 'in_operator'],
+                'Generator Delegation' => ['Generator Delegation', 'generator_delegation']
+            ]
+        ];
+
+        $list2 = $list1;
+
+        $this->assertEmpty($this->diffService->listDiff($list1, $list2));
+    }
+
+    public function testListDiffReturnsCorrectDiff()
+    {
+        $list1 = [
+            'In voting phase' => [
+                'Generator Delegation' => ['Generator Delegation', 'generator_delegation']
+            ],
+            'Accepted' => [
+                'In Operator' => ['In Operator', 'in_operator']
+            ]
+        ];
+
+        $list2 = [
+            'In voting phase' => [
+                'In Operator'          => ['In Operator', 'in_operator'],
+                'Generator Delegation' => ['Generator Delegation', 'generator_delegation']
+            ],
+            'Accepted' => []
+        ];
+
+        $expected = [
+            'In Operator' => [
+                'to'   => 'Accepted',
+                'from' => 'In voting phase'
+            ]
+        ];
+
+        $actual = $this->diffService->listDiff($list1, $list2);
+
+        $this->assertSame($expected, $actual);
+    }
+}
